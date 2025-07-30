d9ae95eca8a69a8289f0de921ebb6725a080dfd0
kaunas163 <ieva.jaksaityte@gmail.com>
2025-07-30 16:15:09 +0300

    route endpoint, returning result from route

--- Project1-Basics-with-simple-routes/ExpressAPI/server.js
+++ Project1-Basics-with-simple-routes/ExpressAPI/server.js
@@ -6,9 +6,18 @@ const port = 3000
 
 app.use(cors())
 
+// http://localhost:3000/
 app.get('/', (req, res) => {
     // res.send('Hello Express!')
-    res.json({message: 'Hello Express with JSON!'})
+    // res.json({message: 'Hello Express with JSON!'})
+
+    // return res.send('first message')
+    // return res.send('second message')
+
+    // res.send('message')
+    // res.json({message: 'some content'})
+
+    res.send({message: 'different message'})
 })
 
 app.listen(port, () => {
