6d2260f716e020a49ca3d047e22f91789562cbbc
kaunas163 <ieva.jaksaityte@gmail.com>
2025-07-30 14:44:19 +0300

    try accessing api from react app

---
+++ Project1-Basics-with-simple-routes/ReactFront/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+  }, [])
+
+  return (
+    <>
+      <p>{message}</p>
+    </>
+  )
+}
+
+export default App
