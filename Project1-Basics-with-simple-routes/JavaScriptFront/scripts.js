746a7eb426f1203dc79c6b9e301f2e9dfb4bebdc
kaunas163 <ieva.jaksaityte@gmail.com>
2025-07-30 17:04:41 +0300

    print data from api to html

--- Project1-Basics-with-simple-routes/JavaScriptFront/scripts.js
+++ Project1-Basics-with-simple-routes/JavaScriptFront/scripts.js
@@ -5,5 +5,8 @@
 fetch('http://localhost:3000')
     .then(response => response.json())
     .then(result => {
-        console.log('DATA FROM API:', result)
+        // console.log('DATA FROM API:', result)
+        document.querySelector('.example-home .content').innerHTML = `
+            <h3>${result.message}</h3>
+        `
     })
