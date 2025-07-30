5fbc06b569aa9db85d19ee6b5fa4f051acdf1dae
kaunas163 <ieva.jaksaityte@gmail.com>
2025-07-30 12:50:59 +0300

    installed nodemon

--- Project1-Basics-with-simple-routes/ExpressAPI/server.js
+++ Project1-Basics-with-simple-routes/ExpressAPI/server.js
@@ -4,9 +4,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Express!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App running on http://localhost:${port}`)
 })
