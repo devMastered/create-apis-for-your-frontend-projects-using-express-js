0289fca26938990766eb2e7ec3d3a4ca1beda7ed
kaunas163 <ieva.jaksaityte@gmail.com>
2025-07-31 17:06:17 +0300

    get data from api using axios

--- Project1-Basics-with-simple-routes/JavaScriptFront/scripts.js
+++ Project1-Basics-with-simple-routes/JavaScriptFront/scripts.js
@@ -2,12 +2,24 @@
 // EXAMPLE - HOME PAGE CONTENT
 // ========================================================
 
-fetch('http://localhost:3000')
-    .then(response => response.json())
+// fetch('http://localhost:3000')
+//     .then(response => {
+//         // console.log(response)
+//         return response.json()
+//     })
+//     .then(result => {
+//         // console.log('DATA FROM API:', result)
+//         document.querySelector('.example-home .content').innerHTML = `
+//             <h3>${result.message}</h3>
+//         `
+//     })
+//     .catch(error => console.log('Error:', error))
+
+axios.get('http://localhost:3000')
     .then(result => {
-        // console.log('DATA FROM API:', result)
+        // console.log('AXIOS RESULT', result)
         document.querySelector('.example-home .content').innerHTML = `
-            <h3>${result.message}</h3>
+            <h3>${result.data.message}</h3>
         `
     })
-    .catch(error => console.log('Error:', error))
+    .catch(error => console.log(error))
