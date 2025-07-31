0a9ed468d50b207bc8d26759d0e04af2031d351f
kaunas163 <ieva.jaksaityte@gmail.com>
2025-07-31 17:25:46 +0300

    use axios in react

--- Project1-Basics-with-simple-routes/ReactFront/src/App.jsx
+++ Project1-Basics-with-simple-routes/ReactFront/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
     const [message, setMessage] = useState('')
 
     useEffect(() => {
-        fetch('http://localhost:3000')
-            .then(response => response.json())
-            .then(data => setMessage(data.message))
+        // fetch('http://localhost:3000')
+        //     .then(response => response.json())
+        //     .then(data => setMessage(data.message))
+
+        axios.get('http://localhost:3000')
+            .then(data => {
+                // console.log(data)
+                setMessage(data.data.message)
+            })
     }, [])
 
     return (
