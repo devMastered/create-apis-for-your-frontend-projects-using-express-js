001e10d02a85b926bab98596c681c3734ddf6bdf
kaunas163 <ieva.jaksaityte@gmail.com>
2025-07-31 17:16:41 +0300

    update html and css in react

--- Project1-Basics-with-simple-routes/ReactFront/src/App.jsx
+++ Project1-Basics-with-simple-routes/ReactFront/src/App.jsx
@@ -2,19 +2,30 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [message, setMessage] = useState('')
+    const [message, setMessage] = useState('')
 
-  useEffect(() => {
-    fetch('http://localhost:3000')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-  }, [])
+    useEffect(() => {
+        fetch('http://localhost:3000')
+            .then(response => response.json())
+            .then(data => setMessage(data.message))
+    }, [])
 
-  return (
-    <>
-      <p>{message}</p>
-    </>
-  )
+    return (
+        <>
+            <div className="block title">
+                <div className="content">
+                    <h2>Simple Routes</h2>
+                </div>
+            </div>
+
+            <div className="block">
+                <p className="block-title">GET /</p>
+                <div className="content">
+                    <h2>{message}</h2>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
