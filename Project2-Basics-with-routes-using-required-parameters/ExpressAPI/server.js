45b25000831924116f1198fe2e4f7ccb4ea51f36
kaunas163 <ieva.jaksaityte@gmail.com>
2025-08-09 17:45:23 +0300

    created files and folders for project 2

---
+++ Project2-Basics-with-routes-using-required-parameters/ExpressAPI/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 3001
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello!')
+})
+
+app.listen(port, () => {
+    console.log(`Project 2 running on http://localhost:${port}`)
+})
