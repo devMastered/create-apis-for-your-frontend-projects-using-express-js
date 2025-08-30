18b8ef27a3ef61c448f9459bf92f088198a68953
kaunas163 <ieva.jaksaityte@gmail.com>
2025-08-30 22:03:53 +0300

    make power of number component work

--- Project2-Basics-with-routes-using-required-parameters/ReactFront/src/components/PowerOfNumber.jsx
+++ Project2-Basics-with-routes-using-required-parameters/ReactFront/src/components/PowerOfNumber.jsx
@@ -1,13 +1,55 @@
+import { useRef, useState } from "react"
 
 const PowerOfNumber = () => {
+    const [numbers, setNumbers] = useState({
+        number: undefined,
+        result: undefined
+    })
+
+    const numberRef = useRef()
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault()
+        // console.log(numberRef)
+        // console.log(numberRef.current.value)
+
+        let num = numberRef.current.value
+
+        if (!num) {
+            alert('Please enter a number!')
+            return
+        }
+
+        // fetch(`http://localhost:3001/power/${num}`)
+
+        fetch('http://localhost:3001/power/' + num)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data)
+                setNumbers(data)
+                numberRef.current.value = ''
+            })
+            .catch(error => console.log('Error:', error))
+    }
+
     return (
         <div className="block">
             <p className="block-title">GET /power/:number</p>
             <div className="content">
-                <p>Please enter a number, to get it's power of 2.</p>
+                {
+                    numbers.result &&
+                    <>
+                        <h3>Number is: {numbers.number}</h3>
+                        <p>It's power: {numbers.result}</p>
+                    </>
+                }
+                {
+                    !numbers.result &&
+                    <p>Please enter a number, to get it's power of 2.</p>
+                }
             </div>
-            <form className="update">
-                <input type="number" min={0} placeholder="Enter a number" />
+            <form onSubmit={formSubmitHandler} className="update">
+                <input ref={numberRef} type="number" min={0} placeholder="Enter a number" />
                 <button type="submit">Get power</button>
             </form>
         </div>
