f3262fc942701e72493093fb509b4bc03bc02d48
kaunas163 <ieva.jaksaityte@gmail.com>
2025-08-30 17:58:27 +0300

    make sum of two numbers block workable

--- Project2-Basics-with-routes-using-required-parameters/JavaScriptFront/scripts.js
+++ Project2-Basics-with-routes-using-required-parameters/JavaScriptFront/scripts.js
@@ -39,3 +39,47 @@ powerBlock.querySelector('.update').addEventListener('submit', (e) => {
         })
         .catch(error => console.log('Error:', error))
 })
+
+// ========================================================
+// EXAMPLE - SUM OF TWO NUMBERS
+// ========================================================
+
+const sumBlock = document.querySelector('.example-sum')
+
+sumBlock.querySelector('.update').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let num1Input = sumBlock.querySelector('input#sumNumber1')
+    let num2Input = sumBlock.querySelector('input#sumNumber2')
+
+    // console.log({num1Input, num2Input})
+
+    if (!num1Input.value || !num2Input.value) {
+        alert('Please enter both numbers!')
+        return
+    }
+
+    // fetch(`http://localhost:3001/sum/${num1Input.value}/${num2Input.value}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         // console.log(data)
+    //         sumBlock.querySelector('.content').innerHTML = `
+    //             <h3>Sum of ${data.number1} and ${data.number2} is: ${data.result}</h3>
+    //         `
+    //         num1Input.value = ''
+    //         num2Input.value = ''
+    //     })
+    //     .catch(error => console.log('Error:', error))
+
+    axios.get(`http://localhost:3001/sum/${num1Input.value}/${num2Input.value}`)
+        .then(response => {
+            // console.log(response)
+            sumBlock.querySelector('.content').innerHTML = `
+                <h3>Sum of ${response.data.number1} and ${response.data.number2} is: ${response.data.result}</h3>
+            `
+            num1Input.value = ''
+            num2Input.value = ''
+        })
+        .catch(error => console.log('Error:', error))
+})
+
