f9626f9840403d361090583c64dcd5cf5209db58
kaunas163 <ieva.jaksaityte@gmail.com>
2025-08-30 16:47:56 +0300

    call power route with javascript

--- Project2-Basics-with-routes-using-required-parameters/JavaScriptFront/scripts.js
+++ Project2-Basics-with-routes-using-required-parameters/JavaScriptFront/scripts.js
@@ -1,5 +1,30 @@
 // ========================================================
-// EXAMPLE
+// EXAMPLE - POWER OF A NUMBER
 // ========================================================
 
+const powerBlock = document.querySelector('.example-power')
 
+powerBlock.querySelector('.update').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const numberInput = powerBlock.querySelector('input#power-number')
+
+    // console.dir(numberInput)
+
+    if (!numberInput.value) {
+        alert('Please enter a number!')
+        return
+    }
+
+    fetch(`http://localhost:3001/power/${numberInput.value}`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data)
+            powerBlock.querySelector('.content').innerHTML = `
+                    <h3>Number is: ${data.number}</h3>
+                    <p>It's power: ${data.result}</p>
+                `
+            numberInput.value = ''
+        })
+        .catch(error => console.log('Error:', error))
+})
