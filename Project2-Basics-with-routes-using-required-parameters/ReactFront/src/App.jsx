92eb08b272076ae2fa37c2d0f6bf0d2155f1f437
kaunas163 <ieva.jaksaityte@gmail.com>
2025-08-30 21:19:44 +0300

    start creating react app

---
+++ Project2-Basics-with-routes-using-required-parameters/ReactFront/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className="block title">
+                <div className="content">
+                    <h2>Routes with Required Parameters</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
